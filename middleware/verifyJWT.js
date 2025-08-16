279aeb7a410b2c6d5e254f32416dfa6bfac9e8e1
pdev6373 <adebayoluborode@gmail.com>
2025-08-16 08:46:16 +0100

    update

--- middleware/verifyJWT.js
+++ middleware/verifyJWT.js
@@ -8,17 +8,13 @@ const verifyJWT = (type) => (req, res, next) => {
 
   const token = authHeader.split(' ')[1];
 
-  verify(
-    token,
-    'rWUAUFHXXmIhNICO6Zlre1mHXn8XivgAOTW6e4BXOLD8sJRSKAISfQ7de8MZE6y12A8ainDlfu3RbuSvBZZhRVWNpZOYdZ/zxd9u115GykkrCQ8eaZuyrMpaL4EUTMQzXyx5TagDvYoQugatuXcLELbAztkXcoo8kmKdl5jGt9QMeG99jT2r742YONzVzGttPbTN5HUFf2yciXBqmD1c+UPbxsskx+XfYhP++jPqYgo5XepJegidrYMZC+owb7blJr110y7G/lDQqljZlGJ0WsMaxu1h5q62aozYFY0B6ta+C7neSn6ru2F7VayN/TMENBcrNRCXq7DR5uEpfasvig==',
-    (err, decoded) => {
-      if (err || decoded.Info.type !== type)
-        return res.status(403).json({ success: false, message: 'Forbidden' });
+  verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err || decoded.Info.type !== type)
+      return res.status(403).json({ success: false, message: 'Forbidden' });
 
-      req.user = decoded.Info;
-      next();
-    },
-  );
+    req.user = decoded.Info;
+    next();
+  });
 };
 
 module.exports = verifyJWT;
